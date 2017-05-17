b6d092bc2ba641fb2f449bf8d69fd6656822d894
Guangyuan Yang <yzgyyang@outlook.com>
2017-05-17 10:56:21 -0400

    Finish the first version of SPI sim

--- freebsd-spi-bb.py
+++ freebsd-spi-bb.py
@@ -1,13 +1,15 @@
+import time
+
 import freebsd-gpio
 
 # Variables
 SS
-SCLK
-MOSI
+SCLK =  # CI (Blue)
+MOSI =  # DI (Green)
 MISO
 
 def delay():
-    pass
+    time.sleep(0.01)
 
 
 def spi_init():
@@ -32,4 +34,39 @@ def spi_write_byte(b):
         set_gpio_value(MOSI, b & (1 << i))
         delay()
         set_gpio_value(SCLK, 1)
+        delay()
 
+
+def spi_write(buf):
+    spi_init()
+    ss_enable(True)
+    delay()
+    for i in buf:
+        spi_write_byte(i)
+    delay()
+    ss_enable(False)
+
+
+def led_on():
+    spi_write_byte(0b00000000)
+    spi_write_byte(0b00000000)
+    spi_write_byte(0b00000000)
+    spi_write_byte(0b00000000)
+
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+    
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+    spi_write_byte(0b11111111)
+
+if __name__ == "__main__":
+    led_on()
