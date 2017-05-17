90dc0ecfba4c4486108f5961a2b56ce813698838
Guangyuan Yang <yzgyyang@outlook.com>
2017-05-17 13:35:35 -0400

    Modify handling binary

--- freebsd_spi_bb.py
+++ freebsd_spi_bb.py
@@ -13,10 +13,10 @@ def delay():
 
 
 def spi_init():
-    set_gpio_direction(SS, "OUT")
+    # set_gpio_direction(SS, "OUT")
     set_gpio_direction(SCLK, "OUT")
     set_gpio_direction(MOSI, "OUT")
-    set_gpio_direction(MISO, "IN")
+    # set_gpio_direction(MISO, "IN")
     set_gpio_value(SCLK, 0)
     set_gpio_value(MOSI, 0)
     
@@ -29,9 +29,9 @@ def ss_enable(flag):
 
 
 def spi_write_byte(b):
-    for i in reversed(xrange(8)):
+    for i in xrange(8):
         set_gpio_value(SCLK, 0)
-        set_gpio_value(MOSI, b & (1 << i))
+        set_gpio_value(MOSI, format(b, "08b")[i])
         delay()
         set_gpio_value(SCLK, 1)
         delay()
