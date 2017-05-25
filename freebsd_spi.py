8f569faff54b6ff75539b261a4176b1d78929299
Guangyuan Yang <yzgyyang@outlook.com>
2017-05-25 12:12:20 -0400

    Switch the interface to fbsd_gpio

--- freebsd_spi.py
+++ freebsd_spi.py
@@ -12,35 +12,24 @@ MOSI = 3 # DI (Green)
 DELAY = 0.01 # Delay time (s)
 
 
-# Gpio value is one of [0, 1, "0", "1"]
-def gpio_set_value(pin, value):
-    gpioc.pin_set(int(pin), int(value))
-
-
-# Direction is one of ["IN", "OUT"]
-def gpio_set_direction(pin, direction):
-    if direction == "OUT":
-        gpioc.pin_output(int(pin))
-
-
 def delay():
     # time.sleep(DELAY)
     pass
 
 
 def spi_init():
-    gpio_set_direction(SCLK, "OUT")
-    gpio_set_direction(MOSI, "OUT")
+    gpioc.pin_output(SCLK)
+    gpioc.pin_output(MOSI)
     gpio_set_value(SCLK, 0)
     gpio_set_value(MOSI, 0)
     
     
 def spi_write_byte(b):
     for i in xrange(8):
-        gpio_set_value(SCLK, 0)
-        gpio_set_value(MOSI, format(b, "08b")[i])
+        gpioc.pin_set(SCLK, 0)
+        gpioc.pin_set(MOSI, int(format(b, "08b")[i]))
         delay()
-        gpio_set_value(SCLK, 1)
+        gpioc.pin_set(SCLK, 1)
 
 
 def spi_write(buf):
