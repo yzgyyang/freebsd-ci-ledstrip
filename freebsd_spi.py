6d52124413704bd14d2f3b3f8af0423af69177ca
Guangyuan Yang <yzgyyang@outlook.com>
2017-05-26 11:45:14 +0000

    Switch to bitwise operator

--- freebsd_spi.py
+++ freebsd_spi.py
@@ -16,16 +16,15 @@ def spi_init():
     gpioc.pin_output(MOSI)
     gpioc.pin_set(SCLK, 0)
     gpioc.pin_set(MOSI, 0)
-    
-    
+
+
 def spi_write_byte(b):
-    for i in xrange(8):
+    for i in xrange(7, -1, -1):
         gpioc.pin_set(SCLK, 0)
-        gpioc.pin_set(MOSI, int(format(b, "08b")[i]))
+        gpioc.pin_set(MOSI, (b >> i) & 1)
         gpioc.pin_set(SCLK, 1)
 
 
 def spi_write(buf):
     for i in buf:
         spi_write_byte(i)
-
